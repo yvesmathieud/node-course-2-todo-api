175b9ab2fe06590abd697c4ad8ba45eaaf45d678
Dathia Nzounkeu <yves.dathianzounkeu@cgi.com>
2019-01-25 12:17:50 -0500

    Test the DELETE /todos/:id route

--- server/server.js
+++ server/server.js
@@ -62,7 +62,7 @@ app.delete('/todos/:id', (req, res) => {
             return res.status(404).send();
         }
 
-        res.status(200).send({todo});
+        res.send({todo});
     }).catch((e) => {
         res.status(400).send();
     });
