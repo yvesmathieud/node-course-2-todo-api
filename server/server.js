cec578f14568d2ecb1977e542514c02020321627
Dathia Nzounkeu <yves.dathianzounkeu@cgi.com>
2019-01-30 17:43:14 -0500

    Add POST /users/login route

--- server/server.js
+++ server/server.js
@@ -104,7 +104,6 @@ app.post('/users', (req, res) => {
 
     user.save().then(() => {
         return user.generateAuthToken();
-        // res.send(user);
     }).then((token) => {
         res.header('x-auth', token).send(user);
     }).catch((e) => {
@@ -114,6 +113,19 @@ app.post('/users', (req, res) => {
 
 app.get('/users/me', authenticate, (req, res) => {
     res.send(req.user);
+});
+
+
+app.post('/users/login', (req, res) => {
+    var body = _.pick(req.body, ['email', 'password']);
+
+    User.findByCredentials(body.email, body.password).then((user) => {
+        return user.generateAuthToken().then ((token) => {
+            res.header('x-auth', token).send(user);
+        });
+    }).catch((e) => {
+        res.status(400).send();
+    });
 });
 
 app.listen(port, () => {
