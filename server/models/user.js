9664c52a3a40c14b98c9dfa6328a3344786905c5
Dathia Nzounkeu <yves.dathianzounkeu@cgi.com>
2019-01-31 12:05:02 -0500

    Add DELETE /users/me/token route

--- server/models/user.js
+++ server/models/user.js
@@ -52,6 +52,16 @@ var UserSchema = new mongoose.Schema({
         });
     };
 
+    UserSchema.methods.removeToken = function (token) {
+        var user = this;
+
+        return user.update({
+            $pull: {
+                tokens: {token}
+            }
+        });
+    };
+
     UserSchema.statics.findByToken = function(token) {
         var User = this;
         var decoded;
